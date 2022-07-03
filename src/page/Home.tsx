6d289a761d08504912e529af39ef9827a20f3ae5
Ben Giese <bengiese22@gmail.com>
2022-07-03 17:51:28 -0600

    Reformat page, add additional payment, clean up file a bit

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,5 @@
-import { Grid, Input, Stack, Typography, FormControl, InputLabel, OutlinedInput, InputAdornment, TextField, Button } from "@mui/material"
 import { useState } from "react"
+import { Grid, Stack, Typography, InputAdornment, TextField, Button } from "@mui/material"
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 import Calculator from "../service/calculator"
 
@@ -32,7 +32,7 @@ const Home = () => {
     const [loanAmount, setLoanAmount] = useState('15000')
     const [monthlyPayment, setMonthlyPayment] = useState('1500')
     const [interestRate, setInterestRate] = useState('0.10')
-
+    const [additionalPayment, setAdditionalPayment] = useState('500')
 
 
     // useEffect(() => {
@@ -43,12 +43,11 @@ const Home = () => {
     const handleButtonClick = () => {
         let calculator = new Calculator()
         const monthBreakdown = calculator.getBreakdownOfLoanPaymentWithAdditionalPayment(
-            Number(loanAmount), 
-            Number(monthlyPayment), 
+            Number(loanAmount),
+            Number(monthlyPayment),
             Number(interestRate),
             Number(500)
         )
-        // console.log(monthBreakdown)
         setData(monthBreakdown)
         setHasData(true)
     }
@@ -64,98 +63,78 @@ const Home = () => {
                     Accelerated Loan Contribution Calculator
                 </Typography>
             </Grid>
-            <Grid item>
-                <Stack direction="column" spacing={2}>
-                    <Grid item>
-                        <Stack direction="row" spacing={4}>
-                            <Stack direction="column" spacing={2}>
-                                {/* <Typography>
-                                    Loan Amount
-                                </Typography> */}
-                                {/* <FormControl fullWidth sx={{ m: 1 }}>
-                                    <InputLabel htmlFor="outlined-adornment-amount">Loan Amount</InputLabel>
-                                    <OutlinedInput
-                                        id="outlined-adornment-amount"
-                                        value={loanAmount}
-                                        onChange={(e) => setLoanAmount(e.target.value)}
-                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                                        label="Amount"
-                                    />
-                                </FormControl> */}
-                                <TextField
-                                    id="loan-amount"
-                                    label="Loan Amount"
-                                    variant="standard"
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                                    }}
-                                    onChange={(e) => setLoanAmount(e.target.value)}
-                                    value={loanAmount}
-                                />
-                            </Stack>
-                            <Stack direction="column" spacing={2}>
-                                {/* <Typography>
-                                    Monthly Payment
-                                </Typography> */}
-                                <TextField
-                                    id="monthly-payment"
-                                    label="Monthly Payment"
-                                    variant="standard"
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                                    }}
-                                    onChange={(e) => setMonthlyPayment(e.target.value)}
-                                    value={monthlyPayment}
-                                />
-                            </Stack>
-                            <Stack direction="column" spacing={2}>
-                                {/* <Typography>
-                                    Interest Rate (%)
-                                </Typography> */}
-                                <TextField
-                                    id="interest-rate"
-                                    label="Interest Rate (%)"
-                                    variant="standard"
-                                    InputProps={{
-                                        endAdornment: <InputAdornment position="end">%</InputAdornment>,
-                                    }}
-                                    onChange={(e) => setInterestRate(e.target.value)}
-                                    value={interestRate}
-                                />
-                            </Stack>
-                            <Stack direction="column" spacing={2}>
-                                <Button variant="contained" color="primary" onClick={handleButtonClick}>
-                                    Show Graph
-                                </Button>
-                            </Stack>
+            <Grid item style={{width: '40vw'}}>
+                <Stack direction="row" spacing={2}>
+                    <Grid item xs={3}>
+                        <Stack direction="column" spacing={2}>
+                            <TextField
+                                id="loan-amount"
+                                label="Loan Amount"
+                                variant="standard"
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                                }}
+                                onChange={(e) => setLoanAmount(e.target.value)}
+                                value={loanAmount}
+                            />
+                            <TextField
+                                id="monthly-payment"
+                                label="Monthly Payment"
+                                variant="standard"
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                                }}
+                                onChange={(e) => setMonthlyPayment(e.target.value)}
+                                value={monthlyPayment}
+                            />
+                            <TextField
+                                id="interest-rate"
+                                label="Interest Rate (%)"
+                                variant="standard"
+                                InputProps={{
+                                    endAdornment: <InputAdornment position="end">%</InputAdornment>,
+                                }}
+                                onChange={(e) => setInterestRate(e.target.value)}
+                                value={interestRate}
+                            />
+                            <TextField
+                                id="additional-payment"
+                                label="Additional Payment"
+                                variant="standard"
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                                }}
+                                onChange={(e) => setAdditionalPayment(e.target.value)}
+                                value={additionalPayment}
+                            />
+                            <Button variant="contained" color="primary" onClick={handleButtonClick}>
+                                Show Graph
+                            </Button>
                         </Stack>
                     </Grid>
-                    {/* <Stack direction="column">
-                        <Input defaultValue="Current Monthly Payment" />
-                        <Input defaultValue="Loan Principal + Any Acrrued Interest" />
-                        <Input defaultValue="Interest Rate" />
-                    </Stack> */}
-                    {hasData === true &&
-                        <LineChart
-                            width={600}
-                            height={400}
-                            data={data}
-                            margin={{
-                                top: 5,
-                                right: 30,
-                                left: 20,
-                                bottom: 5,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" label={"Months"}/>
-                            <YAxis type={"number"} />
-                            <Tooltip />
-                            <Legend />
-                            <Line type="monotone" dataKey="remainingBalance" stroke="#8884d8" activeDot={{ r: 8 }} />
-                            <Line type="monotone" dataKey="additionalPaymentRemainingBalance" stroke="#82ca9d" activeDot={{ r: 8 }} />
-                        </LineChart>
-                    }
+                    <Grid item xs={9}>
+                        {hasData === true &&
+                            <LineChart
+                                width={600}
+                                height={400}
+                                data={data}
+                                margin={{
+                                    top: 5,
+                                    right: 30,
+                                    left: 20,
+                                    bottom: 5,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" label={"Months"} />
+                                <YAxis type={"number"} />
+                                <Tooltip />
+                                <Legend />
+                                <Line type="monotone" dataKey="remainingBalance" stroke="#8884d8" activeDot={{ r: 8 }} />
+                                <Line type="monotone" dataKey="additionalPaymentRemainingBalance" stroke="#82ca9d" activeDot={{ r: 8 }} />
+                            </LineChart>
+                        }
+                    </Grid>
                 </Stack>
             </Grid>
         </Grid>
