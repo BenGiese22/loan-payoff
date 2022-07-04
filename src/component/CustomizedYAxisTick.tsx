a33c39126180d555a22201b5c6cb0ff5ac5f1bdb
Ben Giese <bengiese22@gmail.com>
2022-07-04 14:14:10 -0600

    Add commas to Y Axis ticks

--- src/component/CustomizedYAxisTick.tsx
+++ src/component/CustomizedYAxisTick.tsx
@@ -3,10 +3,14 @@ const CustomizedYAxisTick = (props: any) => {
 
     const { x , y, payload } = props
 
+    const numToStringWithCommas = (value: any) => {
+        return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    }
+
     return (
         <g transform={`translate(${x},${y})`}>
             <text x={0} y={0} dy={16} textAnchor="end" fill="#6e6e6e" transform="rotate(-15)">
-                ${payload.value}
+                ${numToStringWithCommas(payload.value)}
             </text>
         </g>
     )
