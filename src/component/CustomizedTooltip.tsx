860d5830ced28f51d26f5973e51a93c7e8d07c4d
bgiese <bengiese22@gmail.com>
2022-07-13 21:31:28 -0600

    Continue work on tooltip

--- src/component/CustomizedTooltip.tsx
+++ src/component/CustomizedTooltip.tsx
@@ -16,12 +16,18 @@
 const CustomTooltip = (props: any) => {
     const { active, payload, label } = props;
 
+    // console.log(active, payload, label);
+
+    const hasAdditionalPayment = payload.length > 1;
+
+    // TODO process payload values to have comma
+
     if (active && payload && payload.length) {
         return (
             <div className="custom-tooltip">
-                <p className="label">{`${label} : ${payload[0].value}`}</p>
-                <p className="intro">{label}</p>
-                <p className="desc">Anything you want can be displayed here.</p>
+                <p className="date-label">{`Date: ${label}`}</p>
+                <p className="remaining_balance">{`Minimum Payments: $${payload[0].value}`}</p>
+                { hasAdditionalPayment && <p className="additional_payment">{`w/ Additional Payments: $${payload[1].value}`}</p> }
             </div>
         )
     }
