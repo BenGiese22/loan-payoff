5106eff517fa697d527c10f65148c56f13eef58b
Ben Giese <bengiese22@gmail.com>
2022-08-28 20:23:05 -0600

    Remove comments

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -18,13 +18,6 @@ const styles = {
     }
 }
 
-/*
-TODO:
-  - Add buttons to allow N number of loans to be added (up to 5)
-  - Pass these values into calculator get breakdown of loan payments
-  - Update lines to handle N number of loans
-  - Update tooltip to handle N number of loans
-*/
 
 const Home = () => {
 
@@ -129,47 +122,5 @@ const Home = () => {
         </Grid>
     )
 }
-
-/*
-Final Payment Dates
-{
-  additional1: 2022-10-14T23:24:08.339Z,
-  additional0: 2022-11-15T00:24:08.339Z,
-  standard: 2022-12-15T00:24:08.339Z
-}
-
-get first breakdown of payments, see what keys are in them.
-map the keys to N number of lines.
-
-/* 
-<Line name="standard" type="monotone" dataKey="standardRemainingBalance" stroke="#8884d8" activeDot={{ r: 8 }}/>
-<Line name="additional1" type="monotone" dataKey="additional1RemainingBalance" stroke="#82ca9d" activeDot={{ r: 8 }}/>
-
-{
-    additionalPayments.map((payment: any, index: any) => {
-        return (
-            <Stack direction="row" spacing={2} key={index}>
-                <TextField
-                    key={index}
-                    id={`additional-payment-${index}`}
-                    label="Additional Payment"
-                    variant="standard"
-                    placeholder="500"
-                    InputProps={{
-                        startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                    }}
-                    onChange={(e) => updateAdditionalPayment(index, e)}
-                    value={payment.amount}
-                />
-                <ClearTwoToneIcon onClick={() => {
-                    additionalPayments.splice(index, 1)
-                    setAdditionalPayments([...additionalPayments])
-                }} />
-            </Stack>
-        )
-    }
-    )
-}
-*/
 
 export default Home
